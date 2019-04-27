bffbe66661489de676a87da0fa33fd035b3c1576
Minchul <mcwithimp@gmail.com>
2019-04-28 01:58:56 +0900

    Updates

---
+++ precache-manifest.ffcc7497b366a877bc11dcf1afe8d7a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370b00f81bda37c83fcd17d718561244",
+    "url": "/gluwaicoinfo/index.html"
+  },
+  {
+    "revision": "ba486bf81e67202468b2",
+    "url": "/gluwaicoinfo/static/css/2.0120b058.chunk.css"
+  },
+  {
+    "revision": "1a617de532d92be9a257",
+    "url": "/gluwaicoinfo/static/css/main.e5021621.chunk.css"
+  },
+  {
+    "revision": "ba486bf81e67202468b2",
+    "url": "/gluwaicoinfo/static/js/2.b3c2bf55.chunk.js"
+  },
+  {
+    "revision": "1a617de532d92be9a257",
+    "url": "/gluwaicoinfo/static/js/main.1a1296f0.chunk.js"
+  },
+  {
+    "revision": "423d2950bc9fb3578a3f",
+    "url": "/gluwaicoinfo/static/js/runtime~main.ececd293.js"
+  }
+]);
